6c7794f58fa470921b3e48154adf41af42fa1beb
maksudi2804 <maksudiindrarukmana@gmail.com>
2016-11-08 13:59:44 +0700

    f92cef5

--- stringreplace.php
+++ stringreplace.php
@@ -1,8 +1,3 @@
 <?php
 $abc = ' <p><span style="background-color:rgb(249, 249, 249); color:rgb(0, 0, 0); font-family:georgia,times new roman,serif; font-size:14px">4 x&nbsp;</span><span style="background-color:rgb(249, 249, 249); color:rgb(0, 0, 0); font-family:georgia,times new roman,serif; font-size:14px">&pi;r&sup2;</span></p>';
 $result=str_replace('<p>', '', $abc);
-
-echo $result;
-
-
-?>
